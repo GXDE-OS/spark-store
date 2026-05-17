160ce1a94033824c54a8d89b33d2bf16d1c16ded
Azzy Novais <floatingfloatskies@protonmail.com>
2026-05-17 18:58:26 -0400

    Add files via upload

---
+++ src/assets/translations/qtwebengine_pt.ts
@@ -0,0 +1,305 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="es">
+<context>
+    <name>QWebEnginePage</name>
+    <message>
+        <source>Cut</source>
+        <translation>Recortar</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Refazer</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Parar</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Desfazer</translation>
+    </message>
+    <message>
+        <source>&amp;Back</source>
+        <translation>&amp;Voltar</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Colar</translation>
+    </message>
+    <message>
+        <source>Copy Image URL</source>
+        <translation>Copiar URL da imagem</translation>
+    </message>
+    <message>
+        <source>Open Link in This Window</source>
+        <translation>Abrir link nesta janela</translation>
+    </message>
+    <message>
+        <source>Toggle Mute</source>
+        <translation>Ativar/Desativar mudo</translation>
+    </message>
+    <message>
+        <source>Inspect Element</source>
+        <translation>Inspecionar elemento</translation>
+    </message>
+    <message>
+        <source>Toggle Media Controls</source>
+        <translation>Alternar controles de mídia</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Recarregar</translation>
+    </message>
+    <message>
+        <source>Copy Link URL</source>
+        <translation>Copiar URL do link</translation>
+    </message>
+    <message>
+        <source>Exit Full Screen Mode</source>
+        <translation>Sair do modo tela cheia</translation>
+    </message>
+    <message>
+        <source>Paste and Match Style</source>
+        <translation>Colar com o mesmo estilo</translation>
+    </message>
+    <message>
+        <source>Follow Link</source>
+        <translation>Acessar link</translation>
+    </message>
+    <message>
+        <source>Open Link in New Tab</source>
+        <translation>Abrir link em uma nova aba</translation>
+    </message>
+    <message>
+        <source>Reload and Bypass Cache</source>
+        <translation>Recarregar ignorando o cache</translation>
+    </message>
+    <message>
+        <source>Select folder to upload</source>
+        <translation>Selecionar pasta para enviar</translation>
+    </message>
+    <message>
+        <source>Save Image</source>
+        <translation>Salvar imagem</translation>
+    </message>
+    <message>
+        <source>Save Media</source>
+        <translation>Salvar mídia</translation>
+    </message>
+    <message>
+        <source>Toggle Looping</source>
+        <translation>Alternar reprodução contínua</translation>
+    </message>
+    <message>
+        <source>Close Page</source>
+        <translation>Fechar página</translation>
+    </message>
+    <message>
+        <source>Toggle Play/Pause</source>
+        <translation>Alternar Reproduzir/Pausar</translation>
+    </message>
+    <message>
+        <source>Copy Image</source>
+        <translation>Copiar imagem</translation>
+    </message>
+    <message>
+        <source>&amp;Reload</source>
+        <translation>&amp;Recarregar</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>Selecionar tudo</translation>
+    </message>
+    <message>
+        <source>Save Link</source>
+        <translation>Salvar link</translation>
+    </message>
+    <message>
+        <source>Copy Media URL</source>
+        <translation>Copiar URL da mídia</translation>
+    </message>
+    <message>
+        <source>Forward</source>
+        <translation>Avançar</translation>
+    </message>
+    <message>
+        <source>&amp;Forward</source>
+        <translation>&amp;Avançar</translation>
+    </message>
+    <message>
+        <source>Open Link in New Window</source>
+        <translation>Abrir link em uma nova janela</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to leave this page?</source>
+        <translation>Tem certeza de que deseja sair desta página?</translation>
+    </message>
+</context>
+<context>
+    <name>QQuickWebEngineView</name>
+    <message>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Copy Image URL</source>
+        <translation>Copiar URL da imagem</translation>
+    </message>
+    <message>
+        <source>Toggle Mute</source>
+        <translation>Ativar/Desativar mudo</translation>
+    </message>
+    <message>
+        <source>Inspect Element</source>
+        <translation>Inspecionar elemento</translation>
+    </message>
+    <message>
+        <source>Toggle Media Controls</source>
+        <translation>Alternar controles de mídia</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Recarregar</translation>
+    </message>
+    <message>
+        <source>Copy Link URL</source>
+        <translation>Copiar URL do link</translation>
+    </message>
+    <message>
+        <source>Exit Full Screen Mode</source>
+        <translation>Sair do modo tela cheia</translation>
+    </message>
+    <message>
+        <source>Follow Link</source>
+        <translation>Acessar link</translation>
+    </message>
+    <message>
+        <source>Save Image</source>
+        <translation>Salvar imagem</translation>
+    </message>
+    <message>
+        <source>Save Media</source>
+        <translation>Salvar mídia</translation>
+    </message>
+    <message>
+        <source>Toggle Looping</source>
+        <translation>Alternar reprodução contínua</translation>
+    </message>
+    <message>
+        <source>Toggle Play/Pause</source>
+        <translation>Alternar Reproduzir/Pausar</translation>
+    </message>
+    <message>
+        <source>Copy Image</source>
+        <translation>Copiar imagem</translation>
+    </message>
+    <message>
+        <source>Save Link</source>
+        <translation>Salvar link</translation>
+    </message>
+    <message>
+        <source>Copy Media URL</source>
+        <translation>Copiar URL da mídia</translation>
+    </message>
+    <message>
+        <source>Forward</source>
+        <translation>Avançar</translation>
+    </message>
+</context>
+<context>
+    <name>QtWebEngineCore</name>
+    <message>
+        <source>Javascript Alert - %1</source>
+        <translation>Alerta do Javascript - %1</translation>
+    </message>
+    <message>
+        <source>Javascript Confirm - %1</source>
+        <translation>Confirmação do Javascript - %1</translation>
+    </message>
+    <message>
+        <source>Javascript Prompt - %1</source>
+        <translation>Prompt do Javascript - %1</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to leave this page?</source>
+        <translation>Tem certeza de que deseja sair desta página?</translation>
+    </message>
+</context>
+<context>
+    <name>QtWebEnginePlugin</name>
+    <message>
+        <source>Cannot create a separate instance of WebEngineDownloadItem</source>
+        <translation>Não é possível criar uma instância separada de WebEngineDownloadItem</translation>
+    </message>
+    <message>
+        <source>Cannot create separate instance of WebEngineLoadRequest</source>
+        <translation>Não é possível criar uma instância separada de WebEngineLoadRequest</translation>
+    </message>
+    <message>
+        <source>Cannot create a separate instance of WebEngineSettings</source>
+        <translation>Não é possível criar uma instância separada de WebEngineSettings</translation>
+    </message>
+    <message>
+        <source>Cannot create separate instance of WebEngineNavigationRequest</source>
+        <translation>Não é possível criar uma instância separada de WebEngineNavigationRequest</translation>
+    </message>
+    <message>
+        <source>Cannot create separate instance of WebEngineNewViewRequest</source>
+        <translation>Não é possível criar uma instância separada de WebEngineNewViewRequest</translation>
+    </message>
+    <message>
+        <source>Cannot create separate instance of WebEngineCertificateError</source>
+        <translation>Não é possível criar uma instância separada de WebEngineCertificateError</translation>
+    </message>
+    <message>
+        <source>Cannot create a separate instance of FullScreenRequest</source>
+        <translation>Não é possível criar uma instância separada de FullScreenRequest</translation>
+    </message>
+    <message>
+        <source>Cannot create a separate instance of NavigationHistory</source>
+        <translation>Não é possível criar uma instância separada de NavigationHistory</translation>
+    </message>
+</context>
+<context>
+    <name>QtWebEngineExperimentalPlugin</name>
+    <message>
+        <source>Cannot create a separate instance of WebEngineViewExperimental</source>
+        <translation>Não é possível criar uma instância separada de WebEngineViewExperimental</translation>
+    </message>
+    <message>
+        <source>Cannot create a separate instance of WebEngineViewport</source>
+        <translation>Não é possível criar uma instância separada de WebEngineViewport</translation>
+    </message>
+</context>
+<context>
+    <name>UIDelegatesManager</name>
+    <message>
+        <source>Enter username and password for &quot;%1&quot; at %2://%3</source>
+        <translation>Insira o nome de usuário e a senha para &quot;%1&quot; em %2://%3</translation>
+    </message>
+    <message>
+        <source>Connect to proxy &quot;%1&quot; using:</source>
+        <translation>Conectar ao proxy &quot;%1&quot; usando:</translation>
+    </message>
+</context>
+<context>
+    <name>QtWebEngineTestSupportPlugin</name>
+    <message>
+        <source>Cannot create a separate instance of WebEngineErrorPage</source>
+        <translation>Não é possível criar uma instância separada de WebEngineErrorPage</translation>
+    </message>
+</context>
+</TS>
